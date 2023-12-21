ecd6722805a1f9e42e9e74ee919599290262d635
ed1868 <ruizeduardo21@gmail.com>
2023-12-21 14:34:16 -0500

    using tikitoken to count the amount of tokens needed for messages

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ import matplotlib.pyplot as plt
 from IPython.display import display
 
 
+# targetPersonals is an array of targets (needs to be looped)
+targetPersonals = os.environ["TARGETS"];
+
 
 print("Has error module:", hasattr(openai, 'error'))
 
@@ -27,7 +30,7 @@ if hasattr(openai, 'error'):
     print("Has RateLimitError:", hasattr(openai.error, 'RateLimitError'))
 
 
-model = "gpt-3.5-turbo"
+model = "gpt4-turbo"
 load_dotenv(".env")
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -37,3 +40,34 @@ reddit = praw.Reddit(
     client_secret=os.environ["REDDIT_CLIENT_SECRET"],
     user_agent=f"script:test:0.0.1 (by u/yourusername)",
 )
+
+def num_tokens_from_messages(messages, model):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        print("Warning: model not found. Using cl100k_base encoding.")
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo":
+        return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301")
+    elif model == "gpt-4":
+        return num_tokens_from_messages(messages, model="gpt-4-0314")
+    elif model == "gpt-3.5-turbo-0301":
+        tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
+        tokens_per_name = -1  # if there's a name, the role is omitted
+    elif model == "gpt-4-0314":
+        tokens_per_message = 3
+        tokens_per_name = 1
+    else:
+        raise NotImplementedError(
+            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+        )
+    num_tokens = 0
+    for message in messages:
+        num_tokens += tokens_per_message
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":
+                num_tokens += tokens_per_name
+    num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
+    return num_tokens
